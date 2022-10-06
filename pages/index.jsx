fc48b400508355c9d83f7efa91ffdcdddcab28fc
Raul Bernardo Torta Monteiro <raul.monteiro@ciahering.com.br>
2022-10-06 19:29:00 -0300

    add lang to html tag

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,6 +18,7 @@ function Home() {
   return (
     <>
       <Head>
+        <html lang="pt-br" />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-VB8JWPPXDS"
