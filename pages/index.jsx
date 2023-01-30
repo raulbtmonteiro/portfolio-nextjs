4f097c8658f982a9143676b37160be7b061efd32
Raul Bernardo Torta Monteiro <raul.monteiro@ciahering.com.br>
2023-01-30 19:30:29 -0300

    add google tag manager scripts

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,6 +18,17 @@ function Home() {
   return (
     <>
       <Head>
+        {"<!-- Google Tag Manager (noscript) -->"}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PTCR6RX');`
+          }}
+        />
+        {"<!-- End Google Tag Manager (noscript) -->"}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-VB8JWPPXDS"
@@ -36,7 +47,10 @@ function Home() {
         <title>Portfólio - Raul Monteiro</title>
         <link rel="icon" href="/images/iuricode-logo.png" />
         <link rel="apple-touch-icon" href="/images/iuricode-logo.png" />
-        <meta name="description" content="Página de portfólio de Raul Monteiro" />
+        <meta
+          name="description"
+          content="Página de portfólio de Raul Monteiro"
+        />
       </Head>
 
       <GlobalStyle />
